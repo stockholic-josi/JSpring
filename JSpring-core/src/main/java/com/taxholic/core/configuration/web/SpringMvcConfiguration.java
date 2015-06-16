b7f101508e65558920ee2b21597c995455564d69
taxholic <parkjjss2@gmail.com>
2015-06-16 14:37:42 +0900

    1.0.0

---
+++ JSpring-core/src/main/java/com/taxholic/core/configuration/web/SpringMvcConfiguration.java
@@ -0,0 +1,79 @@
+package com.taxholic.core.configuration.web;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+
+@Configuration
+@EnableWebMvc
+//@Import({TilesConfiguration.class, PropertiesConfiguration.class, MessageConfiguration.class, HttpClientConfiguration.class})
+@ComponentScan(basePackages = {"com.taxholic.core.configuration.beans", "com.taxholic.web"}, includeFilters = {
+													@ComponentScan.Filter(Controller.class), @ComponentScan.Filter(ControllerAdvice.class)
+												  }, useDefaultFilters = false)
+public class SpringMvcConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() throws IOException {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:message/server");
+		messageSource.setCacheSeconds(5);
+
+		return messageSource;
+	}
+
+	@Bean
+	public MessageSourceAccessor messageSourceAccessor() throws IOException {
+		return new MessageSourceAccessor(messageSource());
+	}
+	
+    @Bean
+    public MultipartResolver multipartResolver() {
+    	CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+	    resolver.setMaxUploadSize(10485760);
+	    
+	    return resolver;
+    }
+
+    @Bean
+    public ViewResolver ViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setOrder(2);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+    
+    @Bean
+    public UrlBasedViewResolver viewResolver() {
+        UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+        viewResolver.setViewClass(TilesView.class);
+        return viewResolver;
+    }
+    
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[]{
+        		"classpath:config/tiles/tiles-*.xml"
+        });
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
+
+}
